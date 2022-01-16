d0ac228efb79692b7a7ea0333d70cb50724cd4eb
Ya Liu <sarahl0216@gmail.com>
2022-01-16 10:48:34 -0800

    completed exercise_2a

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "Ya", "SoCal", 2, true)
+}
