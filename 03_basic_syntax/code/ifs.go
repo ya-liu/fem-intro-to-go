e4bebd96ebed340bb5ccfe101d3c4fa84205f931
Ya Liu <sarahl0216@gmail.com>
2022-01-16 11:35:42 -0800

    finished if & else content

--- 03_basic_syntax/code/ifs.go
+++ 03_basic_syntax/code/ifs.go
@@ -1,47 +1,47 @@
-// // Uncomment this entire file
+// Uncomment this entire file
 
 package main
 
-// import (
-// 	"fmt",
-// 	"errors"
-// )
+import (
+	"errors"
+	"fmt"
+)
 
-// func someFunction() error {
-// 	return errors.New("some error")
-// }
+func someFunction() error {
+	return errors.New("some error")
+}
 
-// func main() {
+func main() {
 
-// 	var someVar = 9
+	var someVar = 9
 
-// 	if someVar > 10 {
-// 		fmt.Println(someVar)
-// 	}
+	if someVar > 10 {
+		fmt.Println(someVar)
+	}
 
-// 	// 	// ****************************
+	// ****************************
 
-// 	// if someVar > 100 {
-// 	// 	fmt.Println("Greater than 100")
-// 	// } else if someVar == 100 {
-// 	// 	fmt.Println("Equals 100")
-// 	// } else {
-// 	// 	fmt.Println("Less than 100")
-// 	// }
+	if someVar > 100 {
+		fmt.Println("Greater than 100")
+	} else if someVar == 100 {
+		fmt.Println("Equals 100")
+	} else {
+		fmt.Println("Less than 100")
+	}
 
-// 	// 	// ****************************
-// 	// err := someFunction()
-// 	// // => If this function returns a value,
-// 	// // => it will be an  error of type Error
+	// ****************************
+	err := someFunction()
+	// => If this function returns a value,
+	// => it will be an error of type Error
 
-// 	// 	// ****************************
-// 	// if err != nil {
-// 	//   fmt.Println(err.Error())
-// 	// }
+	// ****************************
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-// 	// if err := someFunction(); err != nil {
-// 	//   fmt.Println(err.Error())
-// 	// }
+	if err := someFunction(); err != nil {
+		fmt.Println(err.Error())
+	}
 
-// 	// // End of file curly brace
-// }
+	// // End of file curly brace
+}
