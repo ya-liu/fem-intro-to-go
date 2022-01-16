befd94e18751e2505ffb24f35402600c2563b065
Ya Liu <sarahl0216@gmail.com>
2022-01-16 11:55:26 -0800

    add notes to switch statements

--- 03_basic_syntax/code/switch.go
+++ 03_basic_syntax/code/switch.go
@@ -2,47 +2,49 @@
 
 package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
+func main() {
 
-// 	var city string
+	var city string
 
-// 	switch city {
-// 	case "Des Moines":
-// 		fmt.Println("You live in Iowa")
-// 	case "Minneapolis,", "St Paul":
-// 		fmt.Println("You live in Minnesota")
-// 	case "Madison":
-// 		fmt.Println("You live in Wisconsin")
-// 	default:
-// 		fmt.Println("You're not from around here.")
-// 	}
+	switch city {
+	case "Des Moines":
+		fmt.Println("You live in Iowa")
+	case "Minneapolis,", "St Paul":
+		fmt.Println("You live in Minnesota")
+	case "Madison":
+		fmt.Println("You live in Wisconsin")
+	default:
+		fmt.Println("You're not from around here.")
+	}
 
-// 	// 	// ****************************
-// 	// var i int
+	// // ****************************
+	// var i int
 
-// 	// switch {
-// 	// case i > 10:
-// 	// 	fmt.Println("Greater than 10")
-// 	// case i < 10:
-// 	// 	fmt.Println("Less than 10")
-// 	// default:
-// 	// 	fmt.Println("Is 10")
-// 	// }
+	// switch {
+	// case i > 10:
+	// 	fmt.Println("Greater than 10")
+	// case i < 10:
+	// 	fmt.Println("Less than 10")
+	// default:
+	// 	fmt.Println("Is 10")
+	// }
 
-// 	// 	// ****************************
-// 	// var i int = 9
+	// ****************************
+	var i int = 9
 
-// 	// switch {
-// 	// case i != 10:
-// 	// 	fmt.Println("Does not equal 10")
-// 	// 	fallthrough
-// 	// case i < 10:
-// 	// 	fmt.Println("Less than 10")
-// 	// case i > 10:
-// 	// 	fmt.Println("Greater than 10")
-// 	// default:
-// 	// 	fmt.Println("Is 10")
-// 	// }
-// }
+	switch {
+	case i != 10:
+		fmt.Println("Does not equal 10")
+		fallthrough
+		// the fallthrough statement is a way to control whether to keep the case statement to continue executing code once it reaches something that returns true
+		// here the i is 9, not 10, it will keep going to the next case to evaluate whether i is less than 10
+	case i < 10:
+		fmt.Println("Less than 10")
+	case i > 10:
+		fmt.Println("Greater than 10")
+	default:
+		fmt.Println("Is 10")
+	}
+}
