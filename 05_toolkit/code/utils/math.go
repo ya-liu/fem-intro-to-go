0da82994d6695a3606820b0644c8350a18ebc8be
Ya Liu <sarahl0216@gmail.com>
2022-01-17 00:59:28 -0800

    added notes regarding function naming convention

--- 05_toolkit/code/utils/math.go
+++ 05_toolkit/code/utils/math.go
@@ -2,11 +2,13 @@ package utils
 
 import "fmt"
 
+// this function is not exported from  package utils
 func printNum(num int) {
 	fmt.Println("Current Number: ", num)
 }
 
 // Add function adds multiple numbers
+// Capitalized functions in a package are visible and exported
 func Add(numbers ...int) int {
 	total := 0
 	for _, v := range numbers {
