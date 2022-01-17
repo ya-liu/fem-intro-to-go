339736e3ce45f95495c5a71dcc7b6dfe13de7672
Ya Liu <sarahl0216@gmail.com>
2022-01-16 22:34:40 -0800

    first attempt at part 2

--- 04_complex_structures/code/exercise_4c.go
+++ 04_complex_structures/code/exercise_4c.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Part 1
 func average(numbers ...float64) float64 {
 	total := 0.0
 	for _, number := range numbers {
@@ -10,6 +11,16 @@ func average(numbers ...float64) float64 {
 	return total / float64(len(numbers))
 }
 
+// Part 2
+func found(name string) bool {
+	pets := make(map[string]string)
+	pets["Sprinkles"] = "cat"
+	pets["Chichi"] = "dog"
+	name, ok := pets[name]
+	return ok
+}
+
 func main() {
 	fmt.Println(average(3, 45, 666))
+	fmt.Println(found("Sprinkles"))
 }
