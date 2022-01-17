3f9b32d20be54f9214dcd110c1ac7d8bcea24542
Ya Liu <sarahl0216@gmail.com>
2022-01-17 13:35:53 -0800

    exercise_7a solution and notes

--- 07_pointers/code/exercise_7a.go
+++ 07_pointers/code/exercise_7a.go
@@ -8,18 +8,22 @@ type User struct {
 }
 
 // YOUR CODE HERE
+var u1 = User{
+	ID:        1,
+	FirstName: "Adele",
+	LastName:  "Y",
+	Email:     "adele@adele.com",
+}
+
+// a * next to the type name indicates the variable will point to a memory location
 func updateEmail(user *User) {
 	user.Email = "new@newEmail.com"
 }
 
 func main() {
 	fmt.Println("Pointers!")
-	u1 := User{
-		ID:        1,
-		FirstName: "Adele",
-		LastName:  "Y",
-		Email:     "adele@adele.com",
-	}
+
+	// a & next to the pointer variable name gives you the pointer address
 	updateEmail(&u1)
 	fmt.Println(u1.Email)
 }
