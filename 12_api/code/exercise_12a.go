64266d004713546980e538c52fd466b19f2a6b3c
Ya Liu <sarahl0216@gmail.com>
2022-01-17 21:59:25 -0800

    initialed server and /people route handler

--- 12_api/code/exercise_12a.go
+++ 12_api/code/exercise_12a.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 // BaseURL is the base endpoint for the star wars API
 const BaseURL = "https://swapi.dev/api/"
 
+// create a route for /people
+// write a function that uses fmt.Fprint that indicates the request was successful
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "You've reached the people endpoint!")
+}
+
 func main() {
 	fmt.Println(BaseURL)
+	http.HandleFunc("/people", getPeople)
+	fmt.Println("Server listening on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
