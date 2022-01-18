db13fc112a568e6936fd0b33dc4f0c6c2f93df6d
Ya Liu <sarahl0216@gmail.com>
2022-01-17 21:39:27 -0800

    implemented addTodo function within todos html form

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -43,9 +43,30 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 	err = t.Execute(w, pageVariables)
 }
 
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Request parsing error: ", err)
+	}
+
+	todo := Todo{
+		Title:   r.FormValue("title"),
+		Content: r.FormValue("content"),
+	}
+
+	todos = append(todos, todo)
+	log.Print(todos)
+	// not best practice here, purely for demo
+	// the redirect can let us see the updated todos list
+	http.Redirect(w, r, "/todos/", http.StatusSeeOther)
+}
+
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/todos/", getTodos)
+	http.HandleFunc("/add-todo/", addTodo)
 	fmt.Println("Server is running on port :8080")
 	// log.Fatal is a back-up wrapper for the inner function call in case http.ListenAndServe encounters errors
 	// http.ListenAndServe is run regardless
