cb5d95f98765f2350aee614881c6487b3d73cf04
Ya Liu <sarahl0216@gmail.com>
2022-01-17 22:17:53 -0800

    created structs for working with data returned by api

--- 12_api/code/exercise_12a.go
+++ 12_api/code/exercise_12a.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 // BaseURL is the base endpoint for the star wars API
 const BaseURL = "https://swapi.dev/api/"
+
+type Planet struct {
+	Name       string
+	Terrain    string
+	Population string
+}
+
+type Person struct {
+	Name         string
+	HomeworldURL string
+	Homeworld    Planet
+}
+
+type AllPeople struct {
+	People []Person
+}
 
 // create a route for /people
 // write a function that uses fmt.Fprint that indicates the request was successful
@@ -22,6 +40,23 @@ func getPeople(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(res)
+
+	// ioutil is used to parse the http response for json data
+	// the bytes variable was originally named data
+	// after printing to the console, the returned value is an array of bytes like [5 99 108 101]
+	bytes, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to parse http response")
+	}
+
+	fmt.Println(bytes)
+
+	var people AllPeople
+
+	// works like json.parse to turn the bytes data into a variable of choice, the 2nd argument
+	json.Unmarshal(bytes, &people)
 }
 
 func main() {
