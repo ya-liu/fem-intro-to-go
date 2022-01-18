256cacd212f81a361f3804b452f25e9420fc1bd6
Ya Liu <sarahl0216@gmail.com>
2022-01-17 22:04:17 -0800

    implemented route handler to make an http get request for data

--- 12_api/code/exercise_12a.go
+++ 12_api/code/exercise_12a.go
@@ -13,11 +13,19 @@ const BaseURL = "https://swapi.dev/api/"
 // write a function that uses fmt.Fprint that indicates the request was successful
 
 func getPeople(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "You've reached the people endpoint!")
+	// fmt.Fprint(w, "You've reached the people endpoint!")
+	res, err := http.Get(BaseURL + "people")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to get Star Wars people")
+	}
+
+	fmt.Println(res)
 }
 
 func main() {
-	fmt.Println(BaseURL)
+	// fmt.Println(BaseURL)
 	http.HandleFunc("/people", getPeople)
 	fmt.Println("Server listening on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
