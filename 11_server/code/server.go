3845421a19ab2c13d712d82a684a4f75edbf4b69
Ya Liu <sarahl0216@gmail.com>
2022-01-17 20:19:33 -0800

    implemented a basic server with root route handling

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home!")
+}
 
 func main() {
-	fmt.Println("Building a web server")
+	http.HandleFunc("/", home)
+	fmt.Println("Server is running on port :8080")
+	// log.Fatal is a back-up wrapper for the inner function call in case http.ListenAndServe encounters errors
+	// http.ListenAndServe is run regardless
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
