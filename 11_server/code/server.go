569dfaa5b4fb51d4696870a11a61a88ad9ee7112
Ya Liu <sarahl0216@gmail.com>
2022-01-17 20:48:28 -0800

    implemented variables to modify data rendered on todos template html

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -7,14 +7,32 @@ import (
 	"net/http"
 )
 
+// Todo is a todo with a title and content
+type Todo struct {
+	Title   string
+	Content string
+}
+
+// PageVariables are variables sent to the html template
+type PageVariables struct {
+	PageTitle string
+	PageTodos []Todo
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home!")
 }
 
-func todos(w http.ResponseWriter, r *http.Request) {
+var todos []Todo
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
 	// Fprint prints to the external writer, can be shown on the web page in browser instead of the console
 	// fmt.Fprint(w, "Todos!")
 
+	pageVariables := PageVariables{
+		PageTitle: "Get Todos",
+		PageTodos: todos,
+	}
 	t, err := template.ParseFiles("todos.html")
 
 	if err != nil {
@@ -22,12 +40,12 @@ func todos(w http.ResponseWriter, r *http.Request) {
 		log.Print("Template parsing error: ", err)
 	}
 
-	err = t.Execute(w, nil)
+	err = t.Execute(w, pageVariables)
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/todos/", todos)
+	http.HandleFunc("/todos/", getTodos)
 	fmt.Println("Server is running on port :8080")
 	// log.Fatal is a back-up wrapper for the inner function call in case http.ListenAndServe encounters errors
 	// http.ListenAndServe is run regardless
